78ca5458076834240e66822fe60d6cd089ad5467
RutamBhagat <rutambhagat@gmail.com>
2023-01-30 11:55:32 +0530

    change name of site

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                <div className="container flex flex-wrap items-center justify-between mx-auto">
                   <a href="https://flowbite.com/" className="flex items-center">
                      <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Entertainment District</span>
                   </a>
                   <button
                      data-collapse-toggle="navbar-default"
