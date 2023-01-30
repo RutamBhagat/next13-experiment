5cb2ea2dfd6a0256cef8837477d9f414fcb165e3
RutamBhagat <rutambhagat@gmail.com>
2023-01-30 11:31:07 +0530

    movie page complete

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import Image from "next/image";
 
+
+export async function generateStaticParams(){
+   const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+   );
+   const data = await res.json();
+   return data.results.map(movie => {
+      return {
+         movie: toString(movie.id)
+      }
+   })
+}
+
 const page = async ({ params }) => {
    const { id } = params;
    const imagePath = "https://image.tmdb.org/t/p/original";
@@ -13,16 +26,44 @@ const page = async ({ params }) => {
    const data = await res.json();
 
    return (
-      <div>
-         <div className="px-20 py-12">
-            <h2 className="text-2xl">{data.title}</h2>
-            <h2 className="text-lg">{data.release_date}</h2>
-            <h2 className="">Runtime: {data.runtime} minutes</h2>
-            <h2 className="bg-green-600 inline-block my-2 py-2 px-4 rounded-md text-sm">{data.status}</h2>
-            <Image className="m-12 w-full" priority src={imagePath + data.backdrop_path} alt="" width={1000} height={1000} />
-            <p>{data.overview}</p>
-         </div>
-      </div>
+      <>
+         <section class="bg-white dark:bg-gray-900 min-h-screen   ">
+            <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+               <div class="mr-auto place-self-center lg:col-span-7">
+                  <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                     {data.title}
+                  </h1>
+                  <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                     {data.overview}
+                     <br />
+                     <span>{data.release_date}</span>
+                  </p>
+                  <a
+                     href="#"
+                     class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                  >
+                     Runtime: {data.runtime} min
+                  </a>
+                  <a
+                     href="#"
+                     class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                  >
+                     {data.status}
+                  </a>
+               </div>
+               <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                  <Image
+                     className="m-12 w-full"
+                     priority
+                     src={imagePath + data.backdrop_path}
+                     alt=""
+                     width={1000}
+                     height={1000}
+                  />
+               </div>
+            </div>
+         </section>
+      </>
    );
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -3,15 +3,24 @@ import Movie from "./Movie";
 import shortid from "shortid";
 
 const page = async () => {
-   const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`);
+   const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+   );
    const data = await res.json();
-
    return (
       <main className="flex flex-col justify-center bg-orange-500 px-20 py-12 text-white items-center min-h-screen">
          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {data.results.sort((a, b) => a.title.length - b.title.length).map((movie) => (
-               <Movie key={shortid.generate()} id={movie.id} title={movie.title} poster_path={movie.poster_path} release_date={movie.release_date} />
-            ))}
+            {data.results
+               .sort((a, b) => a.title.length - b.title.length)
+               .map((movie) => (
+                  <Movie
+                     key={shortid.generate()}
+                     id={movie.id}
+                     title={movie.title}
+                     poster_path={movie.poster_path}
+                     release_date={movie.release_date}
+                  />
+               ))}
          </div>
       </main>
    );
